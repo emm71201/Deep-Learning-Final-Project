73c6c947d71942a978a3620b38c3e903fcb776ce
Ubuntu <ubuntu@ip-10-0-0-46.ec2.internal>
2023-11-30 21:37:38 +0000

    attention and data augmentation

---
+++ src/utils/.ipynb_checkpoints/model_utilities_cbam-checkpoint.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        
+        self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_pooled = self.avg_pool(x)
+        max_pooled = self.max_pool(x)
+        
+        avg_out = self.fc2(self.relu1(self.fc1(avg_pooled)))
+        max_out = self.fc2(self.relu1(self.fc1(max_pooled)))
+        out = avg_out + max_out
+        
+        scale = self.sigmoid(out)  # Sigmoid activation
+        return x * scale.expand_as(x)  # Scale the input
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x_cat = torch.cat([avg_out, max_out], dim=1)
+        x_out = self.conv1(x_cat)
+        scale = self.sigmoid(x_out)  # Sigmoid activation
+        return x * scale  # Scale the input
+
+
+class CBAM(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16):
+        super(CBAM, self).__init__()
+        self.ChannelGate = ChannelAttention(gate_channels, reduction_ratio)
+        self.SpatialGate = SpatialAttention()
+
+    def forward(self, x):
+    # Apply Channel Attention
+    channel_attention_map = self.ChannelGate(x)
+    x = x * channel_attention_map.expand_as(x)
+    
+    # Apply Spatial Attention
+    spatial_attention_map = self.SpatialGate(x)
+    # The spatial attention map is 1xHxW, and needs to be broadcasted across the channel dimension
+    # You should not multiply x by x_out again, as it has already been modified by the channel attention
+    x = x * spatial_attention_map.expand_as(x)
+    
+    return x
+
+    
+class CBAMBottleneck(nn.Module):
+    def __init__(self, in_planes, out_planes, stride=1, downsample=None):
+        super(CBAMBottleneck, self).__init__()
+        # Assuming 'out_planes' is 4 times 'in_planes' for a bottleneck
+        self.conv1 = nn.Conv2d(in_planes, in_planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.conv2 = nn.Conv2d(in_planes, in_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(in_planes)
+        self.conv3 = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.cbam = CBAM(out_planes)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+        
+        out = self.cbam(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
--- src/utils/model_utilities_cbam.py
+++ src/utils/model_utilities_cbam.py
@@ -2,28 +2,35 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class ChannelAttention(nn.Module):
     def __init__(self, in_planes, ratio=16):
         super(ChannelAttention, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.max_pool = nn.AdaptiveMaxPool2d(1)
         
-        self.fc1   = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.fc1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
         self.relu1 = nn.ReLU()
-        self.fc2   = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
-
+        self.fc2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
-        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        print("Input size:", x.size())
+        avg_pooled = self.avg_pool(x)
+        max_pooled = self.max_pool(x)
+        print("Avg pooled size:", avg_pooled.size())
+        print("Max pooled size:", max_pooled.size())
+        
+        avg_out = self.fc2(self.relu1(self.fc1(avg_pooled)))
+        max_out = self.fc2(self.relu1(self.fc1(max_pooled)))
         out = avg_out + max_out
-        return self.sigmoid(out)
+        
+        scale = self.sigmoid(out)  # Sigmoid activation
+        return x * scale.expand_as(x)  # Scale the input
 
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
-
         assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
         padding = 3 if kernel_size == 7 else 1
 
@@ -31,26 +38,36 @@ class SpatialAttention(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
+        print("Input size:", x.size())
         avg_out = torch.mean(x, dim=1, keepdim=True)
         max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv1(x)
-        return self.sigmoid(x)
+        print("Avg out size:", avg_out.size())
+        print("Max out size:", max_out.size())
+        x_cat = torch.cat([avg_out, max_out], dim=1)
+        x_out = self.conv1(x_cat)
+        scale = self.sigmoid(x_out)  # Sigmoid activation
+        return x * scale  # Scale the input
 
 
 class CBAM(nn.Module):
-    def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max'], no_spatial=False):
+    def __init__(self, gate_channels, reduction_ratio=16):
         super(CBAM, self).__init__()
         self.ChannelGate = ChannelAttention(gate_channels, reduction_ratio)
-        self.no_spatial = no_spatial
-        if not no_spatial:
-            self.SpatialGate = SpatialAttention()
+        self.SpatialGate = SpatialAttention()
 
     def forward(self, x):
-        x_out = self.ChannelGate(x)
-        if not self.no_spatial:
-            x_out = self.SpatialGate(x_out)
-        return x_out
+    # Apply Channel Attention
+    channel_attention_map = self.ChannelGate(x)
+    x = x * channel_attention_map.expand_as(x)
+    
+    # Apply Spatial Attention
+    spatial_attention_map = self.SpatialGate(x)
+    # The spatial attention map is 1xHxW, and needs to be broadcasted across the channel dimension
+    # You should not multiply x by x_out again, as it has already been modified by the channel attention
+    x = x * spatial_attention_map.expand_as(x)
+    
+    return x
+
     
 class CBAMBottleneck(nn.Module):
     def __init__(self, in_planes, out_planes, stride=1, downsample=None):
