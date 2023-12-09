6c5e653165128e053034b0d6b7aa178fe7c3f066
Edison Murairi <106283492+emm71201@users.noreply.github.com>
2023-12-08 20:14:53 -0500

    summarize codes

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo $"\nRun GMLP, ResNet50 with Metadata, CNN with Attention"
 echo $"\n1. Gated MLP"
 python3 main_code.py gmlp
 echo $"\n2. RESNET50 with Metadata"
-python3 main_code resnet50_model
+python3 main_code.py resnet50_model
 echo $"\n2. CNN with Attention"
-python3 main_code attentioncnn_model
+python3 main_code.py attentioncnn_model
 
